461d5369dd261cca6b761aae0506c53d2a01a082
saltfish0211 <92742009+saltfish0211@users.noreply.github.com>
2022-12-09 11:54:48 +0800

    Create 2008.js

---
+++ 2008.js
@@ -0,0 +1,28 @@
+/*
+糖果NPC腳本
+要用來讓新手升到10等並送去自由市場
+要給他萬能選單
+*/
+
+var status;
+
+function start(){
+  status = -1;
+  action(1,0,0);
+}
+
+function action(mode,type,selection)
+  if (mdoe == 1){
+    status++;
+  }else{
+  status--;
+  }
+  if(status == 0){
+    cm.sendNext("你需要我幫你傳送到自由市場之後並領取新手禮包就點下一頁");
+  }else if(status == 1){
+    cm.sendok("高歌離席~")
+    cm.warp()
+    dm.gainitem()
+    cm.setlevel()
+    cm.dispose();
+  }
