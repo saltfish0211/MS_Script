80e8e3dc706d52dafb7b1120bda7f498be3dfaa2
saltfish0211 <92742009+saltfish0211@users.noreply.github.com>
2022-12-15 09:52:08 +0800

    Update 等級獎勵.js

--- 等級獎勵.js
+++ 等級獎勵.js
@@ -1,7 +1,8 @@
 //等級獎勵
 
+var status ;
 function start () {
-    var status = -1;
+    status = -1;
     action(1,0,0);
   }
   
@@ -21,17 +22,17 @@ function start () {
         text += "" + c + "#b#e#L2# 30等獎勵" + rn;
         cm.sendsimple(text);
     }else if(status == 1){
-        selectionNode = selection
-        WLevel = "等級未達條件"
-        RMessage = "你已經領過了哦"
+        selectionNode = selection;
+        WLevel = "等級未達條件";
+        RMessage = "你已經領過了哦";
         switch(selectionNode){
             case 0:
                 if(PlayerLevel < 10){
-                    text = WLevel
+                    text = WLevel;
                 }else if(PlayerLevel > 10 && cm.getaddpqlog("10等獎勵") == 0){//需要想一個條件來避免重複領取
                     cm.gainitem();
                 }else{
-                    text = RMessage
+                    text = RMessage;
                 }
         }
     }
